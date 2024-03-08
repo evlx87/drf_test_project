f69002dec4e3e95576a37be83c4c2a18872085cc
Sergey Semyonov <evlx@mail.ru>
2024-03-08 21:34:15 +0300

    в приложение Users добавлена модель Payment

--- users/models.py
+++ users/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
+from lms.models import Course, Lesson
+
 # Create your models here.
 NULLABLE = {'blank': True, 'null': True}
+PAYMENT_CHOICES = (('card', 'карта'), ('cash', 'наличные'),)
 
 
 class User(AbstractUser):
@@ -43,3 +46,38 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='пользователь')
+    payment_date = models.DateField(
+        auto_now_add=True,
+        verbose_name='дата оплаты')
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        verbose_name='курс',
+        **NULLABLE)
+    lesson = models.ForeignKey(
+        Lesson,
+        on_delete=models.CASCADE,
+        verbose_name='урок',
+        **NULLABLE)
+    amount = models.PositiveIntegerField(verbose_name='стоимость')
+    payment_method = models.CharField(
+        max_length=50,
+        verbose_name='метод оплаты',
+        choices=PAYMENT_CHOICES,
+        default='card')
+
+    def __str__(self):
+        return f'{
+            self.course if self.course else self.lesson} - {self.payment_date}'
+
+    class Meta:
+        verbose_name = 'платеж'
+        verbose_name_plural = 'платежи'
+        ordering = ('-payment_date',)
